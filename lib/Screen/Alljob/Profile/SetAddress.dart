e4880982836af1176d1a9c265a865f42ff2d88ee
Cosdemon <demonridercos@gmail.com>
2022-06-27 21:50:05 +0700

    all file commit

---
+++ lib/Screen/Alljob/Profile/SetAddress.dart
@@ -0,0 +1,131 @@
+
+import 'package:alljob/Screen/Alljob/Profile/Widgets/AddTextForm.dart';
+import 'package:alljob/Screen/Widgets/ButtonRounded.dart';
+import 'package:flutter/material.dart';
+
+class SetAddress extends StatefulWidget {
+  SetAddress({Key? key}) : super(key: key);
+
+  @override
+  State<SetAddress> createState() => _SetAddressState();
+}
+
+class _SetAddressState extends State<SetAddress> {
+  final GlobalKey<FormState> addFormKey = GlobalKey<FormState>();
+  final TextEditingController username = TextEditingController();
+  List address = [];
+  String _address =
+      '503/32 อาคาร เค.เอส.แอล. ทาวเวอร์, ชั้น 18, ถนนศรีอยุธยา แขวงถนนพญาไท เขตราชเทวี กรุงเทพมหานคร 10400';
+  @override
+  Widget build(BuildContext context) {
+    final padding = EdgeInsets.symmetric(vertical: 5, horizontal: 20);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'ที่อยู่ของคุณ',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 16,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: padding,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('ที่อยู่',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w600)),
+                      ],
+                    ),
+                    ListTile(
+                      onTap: () {},
+                      leading: Icon(
+                        Icons.star,
+                        color: Colors.blue,
+                        size: 35,
+                      ),
+                      title: Text('บริษัท ทดลอง จำกัด'),
+                      subtitle: Text(_address),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        _dialogAddress();
+                      },
+                      child: Center(
+                        child: Text(
+                          '+ เพิ่มที่อยู่',
+                          style: TextStyle(fontSize: 16, color: Colors.blue),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _dialogAddress() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('เพิ่มที่อยู่'),
+          content: SingleChildScrollView(
+            child: Form(
+              key: addFormKey,
+              child: Wrap(
+                children: [
+                  AddTextForm(
+                    controller: username,
+                    hintText: '',
+                    maxLines: 4,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ButtonRounded(
+                  text: 'ยกเลิก',
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                SizedBox(width: 5,),
+                ButtonRounded(
+                  text: 'บันทึก',
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: () {},
+                ),
+                
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
---
+++ lib/Screen/Alljob/Search/SearchScreen.dart
@@ -0,0 +1,119 @@
+
+import 'package:alljob/Screen/Alljob/Search/AddJobRecruitScreen.dart';
+import 'package:alljob/Screen/Alljob/Search/Widgets/CardJobList.dart';
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final GlobalKey<FormState> searchFormKey = GlobalKey<FormState>();
+  final TextEditingController searchController = TextEditingController();
+  List<Map<String, dynamic>> productlist = [
+    {
+      "imgurl": "assets/images/MockupALLZJOB.png",
+      "title": "รับสมัคร โปรแกรมเมอร์",
+      "subtitle": "เงินเดือน 3 หมื่น บริษัท อาชาเทคโนโลยี",
+      "address": "ที่อยู่ ลาดกระบัง กรุงเทพมหานคร",
+      "total": "105.5s",
+      "createAt": "2022-05-06 18:31:50.133920",
+      "hh": "18:31",
+      "dd": "06",
+      "mm": "Feb"
+    }
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ค้นหางาน'),
+      ),
+      body: ListView(
+        children: [
+          buildSearchBar(),
+          CardJobList(list: productlist[0]),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AddJobRecruitScreen()));
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget buildSearchBar() {
+    return Hero(
+      tag: 'tag',
+      child: Container(
+        margin: EdgeInsets.only(left: 20, right: 20, bottom: 16),
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 3),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.white.withOpacity(0.2),
+            ),
+            borderRadius: BorderRadius.circular(10)),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 12, left: 0),
+              child: Icon(Icons.search, color: Colors.deepOrange),
+            ),
+            Expanded(
+              child: Material(
+                color: Colors.white,
+                child: TextField(
+                  controller: searchController,
+                  style: TextStyle(color: Colors.black),
+                  onSubmitted: (value) {},
+                  autofocus: false,
+                  cursorColor: Colors.grey,
+                  decoration: InputDecoration(
+                    hintText: 'Search for home service...',
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.only(
+                    right: 10, left: 10, top: 10, bottom: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  color: Colors.grey.withOpacity(0.1),
+                ),
+                child: Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  spacing: 4,
+                  children: [
+                    Text(
+                      "Filter",
+                      style: TextStyle(
+                          color: Colors
+                              .black), //TextStyle(color: Get.theme.hintColor),
+                    ),
+                    Icon(
+                      Icons.filter_list,
+                      color: Colors.black,
+                      size: 21,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
